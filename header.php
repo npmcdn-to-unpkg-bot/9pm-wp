e6066421ff23f607d9fba9c3f6ade91ead535c14
pengjunp <me@patrickpeng.ca>
2016-07-04 23:06:35 -0700

    initial setup

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+  <div id="wrapper" class="hfeed">
+    <header id="header" role="banner">
+      <section id="branding">
+        <div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
+        <div id="site-description"><?php bloginfo( 'description' ); ?></div>
+      </section>
+      <nav id="menu" role="navigation">
+        <div id="search">
+          <?php get_search_form(); ?>
+        </div>
+        <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+      </nav>
+    </header>
+    <div id="container"> 
+      <div class="grid">
+        <div class="col-3-12">
+          col 3
+        </div>
+        <div class="col-9-12">
+          col 9
+        </div>
+      </div>
