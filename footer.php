4c7a2346502415d86be20dd716bafe7f40d96161
pengjunp <me@patrickpeng.ca>
2016-07-14 23:10:46 -0700

    update footer

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <!-- <div class="clear"></div> -->
 <!-- </div> -->
 <footer id="footer" role="contentinfo">
-  <div id="copyright">
-    <?php echo sprintf( __( '%1$s %2$s %3$s.' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
+  <div class="grid grid-pad">
+    <div id="copyright" class="col-1-1">
+      <?php echo sprintf( __( '%1$s %2$s %3$s.' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
+    </div>
   </div>
 </footer>
 </div>
