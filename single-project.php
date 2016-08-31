e92459f3bffb9b03f20db8a2b8c82231942dfd39
pengjunp <me@patrickpeng.ca>
2016-08-30 19:29:01 -0700

    update index + back button

--- single-project.php
+++ single-project.php
@@ -5,17 +5,22 @@
 get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
+
 <section class="project-container">
   <a href="#" class="menu-trigger">
     <i class="burger"></i>
   </a>
-  
+
   <div class="grid grid-pad">
+
     <div class="col-1-1 mobile-col-1-1">
       <h1><?php echo the_title(); ?></h1>
       <div class="project-description"><?php echo the_content(); ?></div>
     </div>
 
+    <div class="col-1-1 mobile-col-1-1 text-center">
+      <a href="<?php echo home_url(); ?>">Back</a>
+    </div>
 </section>
 
   
